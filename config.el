c1a95b0a791e5b9bbd7d239d4cc43bf86c649156
meidix <mahdihossieni@gmai.com>
2021-07-04 00:18:30 +0430

    added support for Fira Code ligatures support

--- config.el
+++ config.el
@@ -1,45 +1,6 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here! Remember, you do not need to run 'doom
-;; sync' after modifying this file!
-
-
-;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Mehdi Hossieni"
-      user-mail-address "mahdihossieni@gmail.com")
-
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
-;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
-;;   presentations or streaming.
-;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;;(setq doom-font (font-spec :family "monospace" :size 14 :weight 'semi-light)
-       ;doom-variable-pitch-font (font-spec :family "sans" :size 13))
-
-;; There are two ways to load a theme. Both assume the theme is installed and
-;; available. You can either set `doom-theme' or manually load a theme with the
-;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-
-;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
-
-;; Here are some additional functions/macros that could help you configure Doom:
-;;
-;; - `load!' for loading external *.el files relative to this one
-;; - `use-package!' for configuring packages
+;; Place your private configs
 ;; - `after!' for running code after a package has loaded
 ;; - `add-load-path!' for adding directories to the `load-path', relative to
 ;;   this file. Emacs searches the `load-path' when you load packages with
@@ -52,17 +13,12 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
-;;
-;; this is the projectile projects to search for
+(global-fira-code-mode 't)
+(setq doom-font (font-spec :family "Fira Code" :size 14))
+
+;; setting the projectile directories to search for projects
 (setq projectile-project-search-path '("~/projects" "~/Documents/recruites/"))
 
-;; configurring doom to open in full screen mode every time
-;;(add-hook 'window-setup-hook #'toggle-frame-fullscreen)
-
-;; setting the font and ligatures
-(setq doom-font (font-spec :family "Fira Code" :size 16))
-
-;; config for the python-black package
 (use-package python-black
   :demand t
   :after python
